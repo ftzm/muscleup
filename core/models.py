105c8caf929efbdeda4baa5cd5d1e71ce8404e76
matt <fitz.matt.d@gmail.com>
2016-03-31 14:59:17 +0200

    implement all functionality for routinedayslots

--- core/models.py
+++ core/models.py
@@ -30,6 +30,12 @@ class Routine(models.Model):
     @cycle_length.setter
     def cycle_length(self, num):
         self._cycle_length = num
+        for routineday in self.routinedays.all():
+            if routineday.position > num:
+                routineday.position = num
+                routineday.save()
+        if self.cycle_position > num:
+            self.cycle_position = num
 
     @property
     def cycle_position(self):
@@ -64,9 +70,70 @@ class RoutineDay(models.Model):
             pass
             # todo: handle this somehow
 
+    def add_exercise(self, exercise):
+        RoutineDaySlot.objects.create(
+            routineday=self, exercise=exercise)
+
+    def remove_exercise(self, name):
+        routinedayslot = self.routinedayslots.filter(exercise__name=name)
+        if routinedayslot:
+            routinedayslot.delete()
+
+    def close_gap(self, gap):
+        for slot in self.routinedayslots.all():
+            if slot.order > gap:
+                slot._order -= 1
+                slot.save()
+
 
 class RoutineDaySlot(models.Model):
-    pass
+    """
+    relationship model between routineday and exercise,
+    exists for ordering
+    """
+    exercise = models.ForeignKey(
+        Exercise, default=1, on_delete=models.CASCADE,
+        related_name='routinedayslots')
+    _routineday = models.ForeignKey(
+        RoutineDay, default=1, on_delete=models.CASCADE,
+        db_column='routineday',
+        related_name='routinedayslots')
+    _order = models.IntegerField(default=1, db_column="order")
+
+    @property
+    def order(self):
+        return self._order
+
+    @order.setter
+    def order(self, new_order):
+        other_slots = self.routineday.routinedayslots.all()
+        old_order = self._order
+        if new_order > old_order:
+            middle_slots = [s for s in other_slots
+                            if old_order < s.order <= new_order]
+            for slot in middle_slots:
+                slot._order -= 1
+                slot.save()
+        elif new_order < old_order:
+            middle_slots = [s for s in other_slots
+                            if old_order > s.order >= new_order]
+            for slot in middle_slots:
+                slot._order += 1
+                slot.save()
+        self._order = new_order
+
+    @property
+    def routineday(self):
+        return self._routineday
+
+    @routineday.setter
+    def routineday(self, routineday):
+        try:
+            self._order = 1 + \
+                max([r.order for r in routineday.routinedayslots.all()])
+        except ValueError:
+            pass
+        self._routineday = routineday
 
 
 class Workout(models.Model):
--- core/tests.py
+++ core/tests.py
@@ -7,6 +7,10 @@ from core.models import Exercise
 from core.models import Workout
 from core.models import Routine
 from core.models import RoutineDay
+from core.models import RoutineDaySlot
+
+from core.signals import trigger_routinedayslot_gapclose
+from unittest.mock import patch
 
 
 class ExerciseModelTest(TestCase):
@@ -89,6 +93,7 @@ class SetModelTest(TestCase):
 
 
 class RoutineModelTest(TestCase):
+    """todo"""
 
     def test_saving_and_retrieving_routine(self):
         first_routine = Routine()
@@ -133,17 +138,55 @@ class RoutineModelTest(TestCase):
         first_routine.cycle_last_set = \
             datetime.date.today() - datetime.timedelta(1)
         first_routine.save()
+        second_routine = Routine()
+        second_routine.name = "Routine One"
+        second_routine.cycle_length = 7
+        second_routine.cycle_position = 1
+        second_routine.cycle_last_set = \
+            datetime.date.today() - datetime.timedelta(15)
+        second_routine.save()
 
         first_routine.update_cycle_position()
+        second_routine.update_cycle_position()
 
         self.assertEqual(first_routine.cycle_position, 2)
         self.assertEqual(first_routine.cycle_last_set, datetime.date.today())
+        self.assertEqual(second_routine.cycle_position, 2)
+        self.assertEqual(second_routine.cycle_last_set, datetime.date.today())
+
+    def test_keep_cycle_position_inbounds(self):
+        first_routine = Routine()
+        first_routine.name = "Routine One"
+        first_routine.cycle_length = 7
+        first_routine.cycle_position = 7
+
+        first_routine.cycle_length = 3
+
+        self.assertEqual(first_routine.cycle_position, 3)
 
     def test_keep_workoutday_positions_inbounds(self):
-        pass
+        first_routine = Routine()
+        first_routine.name = "Routine One"
+        first_routine.cycle_length = 7
+        first_routine.cycle_position = 1
+        first_routine.cycle_last_set = \
+            datetime.date.today() - datetime.timedelta(1)
+        first_routine.save()
+        first_routineday = RoutineDay()
+        first_routineday.name = "RoutineDay One"
+        first_routineday.routine = Routine.objects.get(name="Routine One")
+        first_routineday.position = 7
+        first_routineday.save()
+
+        first_routine.cycle_length = 3
+        first_routine.save()
+
+        first_routineday = RoutineDay.objects.get(name="RoutineDay One")
+        self.assertEqual(first_routineday.position, 3)
 
 
 class RoutineDayModelTest(TestCase):
+    """todo"""
 
     def setUp(self):
         Routine.objects.create(name="Routine One", cycle_length=7,
@@ -187,23 +230,145 @@ class RoutineDayModelTest(TestCase):
         self.assertEqual(first_routineday.position, 1)
 
     def test_assign_exercises_to_routineday(self):
-        pass
+        routineday = RoutineDay(
+            name="RoutineDay One",
+            routine=Routine.objects.get(name="Routine One"))
+        routineday.save()
+
+        exercise = Exercise.objects.create(name="Situp")
+        routineday.add_exercise(exercise)
+
+        self.assertEqual(routineday.routinedayslots.count(), 1)
+        self.assertEqual(
+            routineday.routinedayslots.first().exercise.name, "Situp")
 
     def test_remove_exercises_from_routineday(self):
-        pass
+        routineday = RoutineDay(
+            name="RoutineDay One",
+            routine=Routine.objects.get(name="Routine One"))
+        routineday.save()
+        exercise = Exercise.objects.create(name="Situp")
+        routineday.add_exercise(exercise)
+
+        routineday.remove_exercise(exercise.name)
+
+        self.assertEqual(routineday.routinedayslots.count(), 0)
 
 
 class RoutineDaySlotModelTest(TestCase):
+    """todo"""
+
+    def setUp(self):
+        first_routine = Routine()
+        first_routine.name = "Routine One"
+        first_routine.cycle_length = 7
+        first_routine.cycle_position = 1
+        first_routine.cycle_last_set = \
+            datetime.date.today() - datetime.timedelta(1)
+        first_routine.save()
+        first_routineday = RoutineDay()
+        first_routineday.name = "RoutineDay One"
+        first_routineday.routine = Routine.objects.get(name="Routine One")
+        first_routineday.position = 3
+        first_routineday.save()
+        first_exercise = Exercise()
+        first_exercise.name = "Pushup"
+        first_exercise.save()
+        second_exercise = Exercise()
+        second_exercise.name = "Plank"
+        second_exercise.save()
+        second_exercise = Exercise()
+        second_exercise.name = "Squat"
+        second_exercise.save()
 
     def test_saving_and_retrieving_routinedayslot(self):
-        pass
-        # new slots increase in order number
+        first_routinedayslot = RoutineDaySlot()
+        first_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        first_routinedayslot.exercise = Exercise.objects.get(name="Pushup")
+        first_routinedayslot.save()
+        second_routinedayslot = RoutineDaySlot()
+        second_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        second_routinedayslot.exercise = Exercise.objects.get(name="Plank")
+        second_routinedayslot.save()
 
-    def test_ordering_slots(self):
-        pass
+        saved_routinedayslots = RoutineDaySlot.objects.all()
+        self.assertEqual(saved_routinedayslots.count(), 2)
+        first_routinedayslot = RoutineDaySlot.objects.all()[0]
+        second_routinedayslot = RoutineDaySlot.objects.all()[1]
 
-    def test_no_slot_gaps(self):
-        pass
+        self.assertEqual(first_routinedayslot.routineday,
+                         RoutineDay.objects.get(name="RoutineDay One"))
+        self.assertEqual(second_routinedayslot.routineday,
+                         RoutineDay.objects.get(name="RoutineDay One"))
+        self.assertEqual(first_routinedayslot.exercise,
+                         Exercise.objects.get(name="Pushup"))
+        self.assertEqual(second_routinedayslot.exercise,
+                         Exercise.objects.get(name="Plank"))
+        self.assertEqual(first_routinedayslot.order, 1)
+        self.assertEqual(second_routinedayslot.order, 2)
+
+    def test_reordering_slots(self):
+        "todo"
+
+        first_routinedayslot = RoutineDaySlot()
+        first_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        first_routinedayslot.exercise = Exercise.objects.get(name="Pushup")
+        first_routinedayslot.save()
+        second_routinedayslot = RoutineDaySlot()
+        second_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        second_routinedayslot.exercise = Exercise.objects.get(name="Plank")
+        second_routinedayslot.save()
+        third_routinedayslot = RoutineDaySlot()
+        third_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        third_routinedayslot.exercise = Exercise.objects.get(name="Squat")
+        third_routinedayslot.save()
+
+        first_routinedayslot.order = 3
+        second_routinedayslot.refresh_from_db()
+        third_routinedayslot.refresh_from_db()
+
+        self.assertEqual(first_routinedayslot.order, 3)
+        self.assertEqual(second_routinedayslot.order, 1)
+        self.assertEqual(third_routinedayslot.order, 2)
+
+        first_routinedayslot.order = 1
+        second_routinedayslot.refresh_from_db()
+        third_routinedayslot.refresh_from_db()
+
+        self.assertEqual(first_routinedayslot.order, 1)
+        self.assertEqual(second_routinedayslot.order, 2)
+        self.assertEqual(third_routinedayslot.order, 3)
+
+    def test_remove_slot_gaps_function(self):
+        first_routinedayslot = RoutineDaySlot()
+        first_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        first_routinedayslot.exercise = Exercise.objects.get(name="Pushup")
+        first_routinedayslot.save()
+        second_routinedayslot = RoutineDaySlot()
+        second_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        second_routinedayslot.exercise = Exercise.objects.get(name="Plank")
+        second_routinedayslot.save()
+        third_routinedayslot = RoutineDaySlot()
+        third_routinedayslot.routineday = RoutineDay.objects.get(
+            name="RoutineDay One")
+        third_routinedayslot.exercise = Exercise.objects.get(name="Squat")
+        third_routinedayslot.save()
+
+        first_routinedayslot.delete()
+        self.assertEqual(RoutineDaySlot.objects.count(), 2)
+
+        second_routinedayslot.refresh_from_db()
+        third_routinedayslot.refresh_from_db()
+
+        self.assertEqual(second_routinedayslot.order, 1)
+        self.assertEqual(third_routinedayslot.order, 2)
 
 
 class ProgressionModelTest(TestCase):
